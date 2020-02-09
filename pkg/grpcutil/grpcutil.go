bebe5544555bfb27d1b9c321c7e07f4b03f1ef7a
4726 <jun28371@gmail.com>
2020-02-09 14:04:21 -0800

    add DefaultServerTLS

--- pkg/grpcutil/grpcutil.go
+++ pkg/grpcutil/grpcutil.go
@@ -6,6 +6,7 @@ import (
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // DefaultServer returns a new grpc server with a logging middleware and a metrics middleware
@@ -22,4 +23,25 @@ func DefaultServer(logEntry *logrus.Entry) *grpc.Server {
 	grpcServer := grpc.NewServer(opts...)
 
 	return grpcServer
+}
+
+func DefaultServerTLS(logEntry *logrus.Entry, tlsCertPath, tlsKeyPath string) (*grpc.Server, error) {
+	creds, err := credentials.NewServerTLSFromFile(tlsCertPath, tlsKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var opts []grpc.ServerOption
+	opts = append(opts, grpc.Creds(creds))
+	opts = append(opts, grpc_middleware.WithUnaryServerChain(
+		grpc_logrus.UnaryServerInterceptor(logEntry),
+		grpc_prometheus.UnaryServerInterceptor,
+	))
+	opts = append(opts, grpc_middleware.WithStreamServerChain(
+		grpc_logrus.StreamServerInterceptor(logEntry),
+		grpc_prometheus.StreamServerInterceptor,
+	))
+	grpcServer := grpc.NewServer(opts...)
+
+	return grpcServer, nil
 }
