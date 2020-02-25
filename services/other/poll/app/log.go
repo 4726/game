a61d6121cac2b13084f49e59fab64b0ba33bb2a1
4726 <jun28371@gmail.com>
2020-02-25 08:58:23 -0800

    fix go modules error

---
+++ services/other/poll/app/log.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logEntry *logrus.Entry
+
+func init() {
+	entry := logrus.WithFields(logrus.Fields{
+		"app": "poll",
+	})
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+
+	logEntry = entry
+}
